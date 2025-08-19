d7ef1e9bd6fd368e2662429e9900ffa4bd8b431c
appyouz <dikpalkhatri7@gmail.com>
2025-08-19 19:19:16 +0545

    feat: viewset for memories display

--- memories/views.py
+++ memories/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Memory
 
 class TimelineView(LoginRequiredMixin, TemplateView):
     template_name = 'memories/timeline.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Fetch all memories from the database and order them
+        context['memories'] = Memory.objects.all().order_by('id')
+        return context
