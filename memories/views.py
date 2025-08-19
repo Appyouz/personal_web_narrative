13e653377f9c95aa969f8aea3538b697d590be1f
appyouz <dikpalkhatri7@gmail.com>
2025-08-19 22:53:13 +0545

    feat: addappreciaton image view anddisplay in timeline.html

--- memories/views.py
+++ memories/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Memory
+from .models import AppreciationImage, Memory
 
 class TimelineView(LoginRequiredMixin, TemplateView):
     template_name = 'memories/timeline.html'
@@ -9,4 +9,7 @@ class TimelineView(LoginRequiredMixin, TemplateView):
         context = super().get_context_data(**kwargs)
         # Fetch all memories from the database and order them
         context['memories'] = Memory.objects.all().order_by('id')
+
+        # Fetch all appreciation images from the database
+        context['appreciation_images'] = AppreciationImage.objects.all().order_by('id')
         return context
