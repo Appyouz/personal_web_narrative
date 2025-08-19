ef958d2e19d7b09819394a45235352cb002c1654
appyouz <dikpalkhatri7@gmail.com>
2025-08-20 00:47:30 +0545

    feat: add audio/video field for cloudinary host

--- memories/admin.py
+++ memories/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Memory, MemoryImage, AppreciationImage
+from .models import Memory, MemoryImage, AppreciationImage, ChapterMusic
 
 class MemoryImageInline(admin.StackedInline):
     model = MemoryImage
@@ -10,3 +10,4 @@ class MemoryAdmin(admin.ModelAdmin):
 
 admin.site.register(Memory, MemoryAdmin)
 admin.site.register(AppreciationImage)
+admin.site.register(ChapterMusic)
--- memories/models.py
+++ memories/models.py
@@ -28,3 +28,18 @@ class AppreciationImage(models.Model):
 
     def __str__(self):
         return self.comment if self.comment else 'Appreciation Image'
+
+
+class ChapterMusic(models.Model):
+    CHAPTER_CHOICES = [
+        ('chapter1', 'Chapter 1'),
+        ('chapter2', 'Chapter 2'),
+        ('chapter3', 'Chapter 3'),
+        ('post-chapter3', 'Post Chapter 3'),
+    ]
+
+    chapter = models.CharField(max_length=20, choices=CHAPTER_CHOICES, unique=True)
+    music_file = CloudinaryField('audio', resource_type='video')  # use 'video' for audio/video files
+
+    def __str__(self):
+        return f"{self.get_chapter_display()}"
