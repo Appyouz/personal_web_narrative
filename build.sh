b708a2721efe6f41dba7b06aa6a43d736fcbeeae
appyouz <dikpalkhatri7@gmail.com>
2025-08-20 15:24:16 +0545

    Create user user through script

--- build.sh
+++ build.sh
@@ -8,5 +8,4 @@ python manage.py migrate
 # Collect static files
 python manage.py collectstatic --noinput
 
-# Create a superuser non-interactively by piping the password
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
+python create_superuser.py
