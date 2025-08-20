34fce41cb5a016e9f34c402b4cfb988f21b55d60
appyouz <dikpalkhatri7@gmail.com>
2025-08-20 15:06:27 +0545

    create tmeporaryuser

--- build.sh
+++ build.sh
@@ -7,3 +7,6 @@ python manage.py migrate
 
 # Collect static files
 python manage.py collectstatic --noinput
+
+# Create a superuser non-interactively by piping the password
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$DJANGO_SUPERUSER_USERNAME', '$DJANGO_SUPERUSER_EMAIL', '$DJANGO_SUPERUSER_PASSWORD')" | python manage.py shell
